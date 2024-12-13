0b67e19266d9e95ac7b272cec7d3a95991d5f892
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-13 11:00:09 +0200

    Resolve ExceptionHandler issue

--- ExceptionHandler.py
+++ ExceptionHandler.py
@@ -2,8 +2,8 @@ from fastapi import HTTPException
 
 
 class ExceptionHandler:
-    def status_code_404():
+    def status_code_404(self):
         return HTTPException(status_code=404, detail={"status": 404, "description": "Requested route doesn't exist", "solution": "Check valid routes in the documentation https://github.com/Svajunas900/FastApiApp"})
     
-    def status_code_400():
+    def status_code_400(self):
         return HTTPException(status_code=400, detail={"status": 400, "description": "Stock name doesn't exist ", "solution": "Check stock name symbols"})
